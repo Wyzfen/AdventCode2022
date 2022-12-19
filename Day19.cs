8a2a8283c99db22aac2d1acc53c3f92206fff770
Matt Lucas <matt.lucas@cambridgeconsultants.com>
2022-12-19 20:17:21 +0000

    Day 19 Part 2

--- Day19.cs
+++ Day19.cs
@@ -41,60 +41,6 @@ namespace AdventCode2022
             return blueprints;
         }
 
-        int RunBlueprintBruteforce(Blueprint blueprint, State state, int maxMinutes, Dictionary<State, int> memos)
-        {
-            int maxGeodes = state.geodes;
-
-            if (state.minutes >= maxMinutes) return maxGeodes;
-
-            int maxGeodeRobots = Math.Min(state.ore / blueprint.geodeRobotOre, state.obsidian / blueprint.geodeRobotObsidian);
-            for (int newGeodeRobots = 0; newGeodeRobots <= maxGeodeRobots; newGeodeRobots++)
-            {
-                int oreAfterGeodeRobots = state.ore - newGeodeRobots * blueprint.geodeRobotOre;
-                int obsidianAfterGeodeRobots = state.obsidian - newGeodeRobots * blueprint.geodeRobotObsidian;
-
-                int maxObsidianRobots = Math.Min(oreAfterGeodeRobots / blueprint.obsidianRobotOre, state.clay / blueprint.obsidianRobotClay);
-                for (int newObsidianRobots = 0; newObsidianRobots <= maxObsidianRobots; newObsidianRobots++)
-                {
-                    int oreAfterObsidianRobots = oreAfterGeodeRobots - newObsidianRobots * blueprint.obsidianRobotOre;
-                    int clayAfterObsidianRobots = state.clay - newObsidianRobots * blueprint.obsidianRobotClay;
-
-                    int maxClayRobots = oreAfterObsidianRobots / blueprint.clayRobotOre;
-                    for (int newClayRobots = 0; newClayRobots <= maxClayRobots; newClayRobots++)
-                    {
-                        int oreAfterClayRobots = oreAfterObsidianRobots - newClayRobots * blueprint.clayRobotOre;
-
-                        int maxOreRobots = oreAfterClayRobots / blueprint.oreRobotOre;
-                        for (int newOreRobots = 0; newOreRobots <= maxOreRobots; newOreRobots++)
-                        {
-                            int oreAfterOreRobots = oreAfterClayRobots - newOreRobots * blueprint.oreRobotOre;
-
-                            var newState = new State(
-                                oreAfterOreRobots + state.oreRobots,
-                                clayAfterObsidianRobots + state.clayRobots,
-                                obsidianAfterGeodeRobots + state.obsidianRobots,
-                                state.geodes + state.geodeRobots,
-                                state.oreRobots + newOreRobots,
-                                state.clayRobots + newClayRobots,
-                                state.obsidianRobots + newObsidianRobots,
-                                state.geodeRobots + newGeodeRobots,
-                                state.minutes + 1);
-
-                            if (!memos.TryGetValue(newState, out int newGeodes))
-                            {
-                                newGeodes = RunBlueprintBruteforce(blueprint, newState, maxMinutes, memos);
-                                memos[newState] = newGeodes;
-                            }
-
-                            maxGeodes = Math.Max(newGeodes, maxGeodes);
-                        }
-                    }
-                }
-            }
-
-            return maxGeodes;
-        }
-
         int CalculateTime(int current, int cost, int perTick)
         {
             int missing = cost - current;
@@ -105,107 +51,116 @@ namespace AdventCode2022
             return 1 + (missing / perTick) + ((missing % perTick) != 0 ? 1 : 0);
         }
 
-        (int geodes, State state) RunBlueprint(Blueprint blueprint, State state, int maxMinutes)
+        State RunBlueprint(Blueprint blueprint, State state, int maxMinutes)
         {
-            int maxGeodes = state.geodes;
             var maxState = state;
 
-            if (state.minutes >= maxMinutes) return (maxGeodes, state);
+            if (state.minutes >= maxMinutes) return state;
 
             // each step, there's 4 robots that could be built, so test each
-            int time = CalculateTime(state.ore, blueprint.oreRobotOre, state.oreRobots);
-            if(time != int.MaxValue && state.minutes + time < maxMinutes)
-            {
-                var (newGeodes, newState) = RunBlueprint(blueprint, state with 
-                                            { 
-                                                minutes = state.minutes + time, 
-                                                ore = state.ore + time * state.oreRobots - blueprint.oreRobotOre, 
-                                                clay = state.clay + time * state.clayRobots,
-                                                oreRobots = state.oreRobots + 1
-                                            }, 
-                                            maxMinutes);
-
-                if(newGeodes > maxGeodes)
-                {
-                    maxGeodes = newGeodes;
-                    maxState = newState;
-                }
-            }
-
-            time = CalculateTime(state.ore, blueprint.clayRobotOre, state.oreRobots);
+            // There's no point building more of a robot, than the maximum that can be used in one turn
+            #region Geode
+            int time = Math.Max(CalculateTime(state.ore, blueprint.geodeRobotOre, state.oreRobots),
+                                CalculateTime(state.obsidian, blueprint.geodeRobotObsidian, state.obsidianRobots));
             if (time != int.MaxValue && state.minutes + time < maxMinutes)
             {
-                var (newGeodes, newState) = RunBlueprint(blueprint, state with 
-                                            {
-                                                minutes = state.minutes + time, 
-                                                ore = state.ore + time * state.oreRobots - blueprint.clayRobotOre,
-                                                clay = state.clay + time * state.clayRobots,
-                                                obsidian = state.obsidian + time * state.obsidianRobots,
-                                                clayRobots = state.clayRobots + 1 
-                                            },
-                                            maxMinutes);
-
-                if (newGeodes > maxGeodes)
+                var newState = RunBlueprint(blueprint, state with
                 {
-                    maxGeodes = newGeodes;
+                    minutes = state.minutes + time,
+                    ore = state.ore + time * state.oreRobots - blueprint.geodeRobotOre,
+                    clay = state.clay + time * state.clayRobots,
+                    obsidian = state.obsidian + time * state.obsidianRobots - blueprint.geodeRobotObsidian,
+                    geodeRobots = state.geodeRobots + 1,
+                    geodes = state.geodes + (maxMinutes - state.minutes - time) // generate all the geodes now
+                }, maxMinutes);
+
+                if (newState.geodes > maxState.geodes)
+                {
                     maxState = newState;
                 }
             }
+            #endregion
 
-            time = Math.Max(CalculateTime(state.ore, blueprint.obsidianRobotOre, state.oreRobots),
-                            CalculateTime(state.clay, blueprint.obsidianRobotClay, state.clayRobots));
-            if (time != int.MaxValue && state.minutes + time < maxMinutes)
+            #region Obsidian
+            if (state.obsidianRobots < blueprint.geodeRobotObsidian)
             {
-                var (newGeodes, newState) = RunBlueprint(blueprint, state with 
-                                            {
-                                                minutes = state.minutes + time, 
-                                                ore = state.ore + time * state.oreRobots - blueprint.obsidianRobotOre, 
-                                                clay = state.clay + time * state.clayRobots - blueprint.obsidianRobotClay, 
-                                                obsidian = state.obsidian + time * state.obsidianRobots,
-                                                obsidianRobots = state.obsidianRobots + 1
-                                            }, 
-                                            maxMinutes);
-
-                if (newGeodes > maxGeodes)
+                time = Math.Max(CalculateTime(state.ore, blueprint.obsidianRobotOre, state.oreRobots),
+                                CalculateTime(state.clay, blueprint.obsidianRobotClay, state.clayRobots));
+                if (time != int.MaxValue && state.minutes + time < maxMinutes)
                 {
-                    maxGeodes = newGeodes;
-                    maxState = newState;
+                    var newState = RunBlueprint(blueprint, state with
+                    {
+                        minutes = state.minutes + time,
+                        ore = state.ore + time * state.oreRobots - blueprint.obsidianRobotOre,
+                        clay = state.clay + time * state.clayRobots - blueprint.obsidianRobotClay,
+                        obsidian = state.obsidian + time * state.obsidianRobots,
+                        obsidianRobots = state.obsidianRobots + 1
+                    },  maxMinutes);
+
+                    if (newState.geodes > maxState.geodes)
+                    {
+                        maxState = newState;
+                    }
                 }
             }
+            #endregion
 
-            time = Math.Max(CalculateTime(state.ore, blueprint.geodeRobotOre, state.oreRobots),
-                            CalculateTime(state.obsidian, blueprint.geodeRobotObsidian, state.obsidianRobots));
-
-            if (time != int.MaxValue && state.minutes + time < maxMinutes)
+            #region Clay
+            if (state.clayRobots < blueprint.obsidianRobotClay)
             {
-                var (newGeodes, newState) = RunBlueprint(blueprint, state with
-                                            {
-                                                minutes = state.minutes + time,
-                                                ore = state.ore + time * state.oreRobots - blueprint.geodeRobotOre,
-                                                clay = state.clay + time * state.clayRobots,
-                                                obsidian = state.obsidian + time * state.obsidianRobots - blueprint.geodeRobotObsidian,
-                                                geodeRobots = state.geodeRobots + 1,
-                                                geodes = state.geodes + (maxMinutes - state.minutes - time) // gnerate all the geodes now
-                                            },
-                                            maxMinutes);
-
-                if (newGeodes > maxGeodes)
+                time = CalculateTime(state.ore, blueprint.clayRobotOre, state.oreRobots);
+                if (time != int.MaxValue && state.minutes + time < maxMinutes)
                 {
-                    maxGeodes = newGeodes;
-                    maxState = newState;
+                    var newState = RunBlueprint(blueprint, state with
+                    {
+                        minutes = state.minutes + time,
+                        ore = state.ore + time * state.oreRobots - blueprint.clayRobotOre,
+                        clay = state.clay + time * state.clayRobots,
+                        obsidian = state.obsidian + time * state.obsidianRobots,
+                        clayRobots = state.clayRobots + 1
+                    },  maxMinutes);
+
+                    if (newState.geodes > maxState.geodes)
+                    {
+                        maxState = newState;
+                    }
                 }
             }
+            #endregion
 
-            return (maxGeodes, maxState);
+            #region Ore
+            if (state.oreRobots < new[] { blueprint.oreRobotOre, blueprint.clayRobotOre, blueprint.obsidianRobotOre, blueprint.geodeRobotOre }.Max())
+            {
+                time = CalculateTime(state.ore, blueprint.oreRobotOre, state.oreRobots);
+                if (time != int.MaxValue && state.minutes + time < maxMinutes)
+                {
+                    var newState = RunBlueprint(blueprint, state with
+                    {
+                        minutes = state.minutes + time,
+                        ore = state.ore + time * state.oreRobots - blueprint.oreRobotOre,
+                        clay = state.clay + time * state.clayRobots,
+                        oreRobots = state.oreRobots + 1
+                    }, maxMinutes);
+
+                    if (newState.geodes > maxState.geodes)
+                    {
+                        maxState = newState;
+                    }
+                }
+            }
+            #endregion
+
+            return maxState;
         }
 
         [TestMethod]
         public void Problem1()
         {
             var blueprints = ParseInput(values);
-            var results = blueprints.Select(b => RunBlueprint(b, new State(0, 0, 0, 0, 1, 0, 0, 0, 0), 24).geodes).ToList();
+            var results = blueprints.Select(b => RunBlueprint(b, new State(0, 0, 0, 0, 1, 0, 0, 0, 0), 24)).ToList();
+            var zip = blueprints.Zip(results).ToList();
 
-            int result = results.Select((g, i) => (i + 1) * g).Sum();
+            int result = results.Select((g, i) => (i + 1) * g.geodes).Sum();
 
             Assert.AreEqual(result, 1528);
         }
@@ -213,12 +168,12 @@ namespace AdventCode2022
         [TestMethod]
         public void Problem2()
         {
-            var blueprints = ParseInput(values).Take(3);
+            var blueprints = ParseInput(values).Take(3).ToList();
             var results = blueprints.Select(b => RunBlueprint(b, new State(0, 0, 0, 0, 1, 0, 0, 0, 0), 32).geodes).ToList();
 
             int result = results.Aggregate(1, (a, b) => a * b);
 
-            Assert.AreEqual(result, 4267809);
+            Assert.AreEqual(result, 16926);
         }
     }
 }
